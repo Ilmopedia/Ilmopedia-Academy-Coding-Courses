b5b538daaf014e08325e26f85bdf2e280e47e199
ZI Coder <82158378+ZI-Youtuber@users.noreply.github.com>
2022-02-19 21:05:22 +0000

    Create hexagon.py

---
+++ Drawing Shapes using Code (Python Turtle)/hexagon.py
@@ -0,0 +1,14 @@
+import turtle 
+
+turtle.forward(70)
+turtle.left(60)
+turtle.forward(70)
+turtle.left(60)
+turtle.forward(70)
+turtle.left(60)
+turtle.forward(70)
+turtle.left(60)
+turtle.forward(70)
+turtle.left(60)
+turtle.forward(70)
+turtle.left(60)
